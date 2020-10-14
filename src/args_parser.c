aa6acba0020f7d7913b53416b98a026e19e85993
Rafael Campos Nunes <rafaelnunes@engineer.com>
2020-10-13 23:33:32 -0300

    Implement the argument parser
    Now the argument parser is complete, with room to improvement of course.
    
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/args_parser.c
+++ src/args_parser.c
@@ -1,21 +1,39 @@
 #include "args_parser.h"
 
+#include "vm/lib/vm_string.h"
+
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 args_t args_parse(int argc, char **argv) {
     args_t parsed;
 
-    parsed.flags[0].flag_name = "";
-    parsed.flags[0].flag_value = "";
-    parsed.flags[1].flag_name = "";
-    parsed.flags[1].flag_value = "";
+    // Allocate a buffer big enough to hold every argument passed.
+    // FIXME: THIS IS INSECURE.
+    char *all_args = calloc(sizeof(char), 8196);
 
+    for (size_t i = 1; i < argc; i++) {
+        strcat(strcat(all_args, " "), argv[i]);
+    }
 
-    for(int i = 0; i < 4 && i < argc-1; i = i + 2) {
-        parsed.flags[i].flag_name = argv[i];
+    for (size_t i = 0; i < argc-1; i++) {
+        char *flag = vm_strspliti(all_args, " ", i);
 
-        if (i + 1 < argc - 1) {
-            parsed.flags[i].flag_value = argv[i+1];
+        parsed.flags[i].flag_name = flag;
+
+        if (i+1 < argc-1) {
+            char *value = vm_strspliti(all_args, " ", i+1);
+
+            if (vm_strncmp(value, "--", 2)) {
+                parsed.flags[i].flag_value = value;
+                // if this is a value then skip the next check
+                ++i;
+
+                printf("%s %s\n", flag, value);
+            } else {
+                parsed.flags[i].flag_value = "";
+            }
         }
     }
 
