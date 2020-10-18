a34c8bd0517dbde1fd9cd44b5a4f228d8d1b4fa0
Vinícius Gomes de Souza <vgdsouza@live.com>
2020-10-18 16:29:12 -0300

    Code and Constant Value implemented
    Attribute Code and Attribute Constant Value implemented
    
    Signed-off-by: Vinícius Gomes de Souza <vgdsouza@live.com>

--- src/vm/attribute_types/vm_code.h
+++ src/vm/attribute_types/vm_code.h
@@ -1,4 +1,38 @@
 #ifndef VM_CODE_H
 #define VM_CODE_H
 
+#include <stdint.h>
+#include "../vm_constant_pool.h"
+
+/**
+ * @brief
+ */
+typedef struct vm_exception_table
+{
+    uint16_t start_pc;
+    uint16_t end_pc;
+    uint16_t handler_pc;
+    uint16_t catch_type;
+} vm_exception_table_t;
+
+/**
+ * @brief
+ */
+typedef struct vm_code
+{
+    uint16_t attribute_name_index;
+    uint32_t attribute_length;
+    uint16_t max_stack;
+    uint16_t max_local;
+
+    uint32_t code_length;
+    uint8_t *code;
+
+    uint16_t exception_table_length;
+    vm_exception_table_t *exception_table;
+
+    uint16_t attributes_count;
+    vm_attribute_info_t *attributes;
+} vm_code_t;
+
 #endif /* VM_CODE_H */
--- src/vm/attribute_types/vm_constant_value.h
+++ src/vm/attribute_types/vm_constant_value.h
@@ -1,4 +1,16 @@
 #ifndef VM_CONSTANT_VALUE_H
 #define VM_CONSTANT_VALUE_H
 
+#include <stdint.h>
+
+/**
+ * @brief
+ */
+typedef struct vm_constant_value
+{
+    uint16_t attribute_name_index;
+    uint32_t attribute_length;
+    uint16_t constantvalue_index;
+} vm_constant_value_t;
+
 #endif /* VM_CONSTANT_VALUE_H */
