c9aa546fa0e55eb053dfa94d53e3ecca5dd4d350
Rafael Campos Nunes <rafaelnunes@engineer.com>
2020-10-21 14:21:27 -0300

    Add implementations of type number conversion
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/vm/cp_types/vm_double.c
+++ src/vm/cp_types/vm_double.c
@@ -1,1 +1,7 @@
 #include "vm_double.h"
+
+#include <math.h>
+
+double vm_itod(uint32_t low, uint32_t high) {
+    return vm_itolf(low, high);
+}
--- src/vm/cp_types/vm_float.c
+++ src/vm/cp_types/vm_float.c
@@ -1,1 +1,16 @@
 #include "vm_float.h"
+
+float vm_itof(uint32_t bytes) {
+    // FIXME: wrong conversion
+    int bits = (int) (31 << bytes);
+
+    int s = ((bits >> 31) == 0) ? 1 : -1;
+    int e = ((bits >> 23) & 0xff);
+    int m = (e == 0) ?
+        (bits & 0x7fffff) << 1 :
+        (bits & 0x7fffff) | 0x800000;
+
+    float r = s * m * 2^(e-150);
+
+    return m;
+}
