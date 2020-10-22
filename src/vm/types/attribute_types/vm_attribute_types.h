3a2409088fa0710710806def9f7c404de043e699
Vinícius Gomes de Souza <vgdsouza@live.com>
2020-10-22 14:43:29 -0300

    vm_attribute_types.h created for easier imports
    File created for easier import of attribute_types.
    
    Signed-off-by: Vinícius Gomes de Souza <vgdsouza@live.com>

--- src/vm/types/attribute_types/vm_attribute_types.h
+++ src/vm/types/attribute_types/vm_attribute_types.h
@@ -1,6 +1,28 @@
 #ifndef VM_ATTRIBUTE_TYPES_H
 #define VM_ATTRIBUTE_TYPES_H
 
-
+#include "vm_constant_value.h"
+#include "vm_code.h"
+#include "vm_stack_map_table.h"
+#include "vm_exceptions.h"
+#include "vm_inner_classes.h"
+#include "vm_enclosing_method.h"
+#include "vm_synthetic.h"
+#include "vm_signature.h"
+#include "vm_source_file.h"
+#include "vm_source_debug_extension.h"
+#include "vm_line_number_table.h"
+#include "vm_local_variable_table.h"
+#include "vm_local_variable_type_table.h"
+#include "vm_deprecated.h"
+#include "vm_runtime_visible_annotations.h"
+#include "vm_runtime_invisible_annotations.h"
+#include "vm_runtime_visible_parameter_annotations.h"
+#include "vm_runtime_invisible_parameter_annotations.h"
+#include "vm_runtime_visible_type_annotations.h"
+#include "vm_runtime_invisible_type_annotations.h"
+#include "vm_annotation_default.h"
+#include "vm_bootstrap_methods.h"
+#include "vm_method_parameters.h"
 
 #endif /* VM_ATTRIBUTE_TYPES_H */
