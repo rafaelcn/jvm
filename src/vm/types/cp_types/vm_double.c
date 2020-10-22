c9956bf1fb841229121461aa5a28b8fe15742c3b
Rafael Campos Nunes <rafaelnunes@engineer.com>
2020-10-22 17:49:39 -0300

    Try to fix the conversion implementation
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/vm/types/cp_types/vm_double.c
+++ src/vm/types/cp_types/vm_double.c
@@ -1,7 +1,5 @@
 #include "vm_double.h"
 
-#include <math.h>
-
 double vm_itod(uint32_t low, uint32_t high) {
-    return vm_itolf(low, high);
+    return (double)vm_itolf(low, high);
 }
--- src/vm/types/cp_types/vm_long.c
+++ src/vm/types/cp_types/vm_long.c
@@ -1,17 +1,18 @@
 #include "vm_long.h"
 
 #include <stdint.h>
+#include <math.h>
 
 long vm_itolf(uint32_t low, uint32_t high) {
     long bits = ((long) high << 32) + low;
 
     // use another function to test for NaN and INFINITY
-    if (bits == 0x7ff0000000000000L ||
+    /*if (bits == 0x7ff0000000000000L ||
         bits == 0xfff0000000000000L ||
         (bits >= 0x7ff0000000000001L && bits <= 0x7fffffffffffffffL) ||
         (bits >= 0xfff0000000000001L && bits <= 0xffffffffffffffffL)) {
         return bits;
-    }
+    }*/
 
     int s = ((bits >> 63) == 0) ? 1 : -1;
     int e = (int)((bits >> 52) & 0x7ffL);
@@ -19,5 +20,9 @@ long vm_itolf(uint32_t low, uint32_t high) {
                 (bits & 0xfffffffffffffL) << 1 :
                 (bits & 0xfffffffffffffL) | 0x10000000000000L;
 
-    return m;
+    long long result = s * m * pow(m, (e-1075));
+
+    printf(" | s = %d, e = %d, m = %ld, r = %lld | ", s, e, m, result);
+
+    return result;
 }
