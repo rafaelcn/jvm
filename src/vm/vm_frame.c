568b3c917e0de9aad87f8a9c2e1ae2455a4174e3
Vinícius Gomes de Souza <vgdsouza@live.com>
2020-11-08 16:36:31 -0300

    Moving some things arround
    Signed-off-by: Vinícius Gomes de Souza <vgdsouza@live.com>

--- src/vm/vm_frame.c
+++ src/vm/vm_frame.c
@@ -1,11 +1,71 @@
-#include vm_frame.h
+#include "vm_frame.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <math.h>
+/**
+ * @brief Pushes a stack_frame into the JVM Stack.
+ * @param stack A pointer to the JVM Stack.
+ * @param new_frame A pointer to the a new frame.
+ * @returns Nothing.
+ */
+void push_into_stack(vm_stack_t *stack, vm_stack_frame_t *new_frame) {
+    // the new_frame points towards the former top_frame
+    new_frame->next_frame = stack->top_frame;
+    // the new_frame is the new top_frame
+    stack->top_frame = new_frame;
+    // the frames_count is increased
+    stack->frames_count += 1;
 
-#include "vm_file.h"
-#include "utils/vm_target.h"
-#include "lib/vm_string.h"
+    return;
+}
+
+/**
+ * @brief Pushes a operand_stack_frame into the Operands Stack.
+ * @param stack A pointer to the Operands Stack.
+ * @param new_frame A pointer to the a new frame.
+ * @returns Nothing.
+ */
+void push_into_ostack(vm_operand_stack_t *stack, vm_operand_stack_frame_t *new_frame) {
+    // the new_frame points towards the former top_frame
+    new_frame->next_frame = stack->top_frame;
+    // the new_frame is the new top_frame
+    stack->top_frame = new_frame;
+    // the frames_count is increased
+    stack->frames_count += 1;
+
+    return;
+}
+
+/**
+ * @brief Pops the frame at the top of the JVM Stack.
+ * @param stack A pointer to the JVM Stack.
+ * @returns A pointer to the popped frame.
+ */
+vm_stack_frame_t * pop_from_stack(vm_stack_t *stack) {
+    // temporarily holds the former top_frame
+    vm_stack_frame_t *popped_frame = stack->top_frame;
+    // a new top_frame is assigned to the stack
+    stack->top_frame = popped_frame->next_frame;
+    // the frames_count is decreased
+    stack->frames_count -= 1;
+    // the popped_frame points to NULL
+    popped_frame->next_frame = NULL;
+
+    return popped_frame;
+}
+
+/**
+ * @brief Pops the frame at the top of the Operands Stack.
+ * @param stack A pointer to the Operands Stack.
+ * @returns A pointer to the popped frame.
+ */
+vm_operand_stack_frame_t * pop_from_ostack(vm_operand_stack_t *stack) {
+    // temporarily holds the former top_frame
+    vm_operand_stack_frame_t *popped_frame = stack->top_frame;
+    // a new top_frame is assigned to the stack
+    stack->top_frame = popped_frame->next_frame;
+    // the frames_count is increased
+    stack->frames_count -= 1;
+    // the popped_frame points to NULL
+    popped_frame->next_frame = NULL;
+
+    return popped_frame;
+}
--- src/vm/vm_frame.h
+++ src/vm/vm_frame.h
@@ -1,42 +1,68 @@
 #ifndef VM_FRAME_H
 #define VM_FRAME_H
 
-#include <stdlib.h>
 #include <stdint.h>
 
-#include "vm_file.h"
-#include "types/cp/vm_cp_types.h"
-#include "types/attributes/vm_attribute_types.h"
 #include "vm_constant_pool.h"
 
+typedef struct vm_stack_frame vm_stack_frame_t;
+typedef struct vm_operand_stack_frame vm_operand_stack_frame_t;
+typedef struct vm_local_variable_item vm_local_variable_item_t;
+
+/**
+ * @brief
+ */
+struct vm_local_variable_item {
+    // some form of value here
+    vm_local_variable_item_t *next_item;
+};
+
+/**
+ * @brief
+ */
+typedef struct vm_local_variables_list {
+    uint16_t local_variables_count;
+    vm_local_variable_item_t *first_item;
+} vm_local_variables_list_t;
+
+/**
+ * @brief
+ */
+struct vm_operand_stack_frame {
+    // some form of value here
+    vm_operand_stack_frame_t *next_frame;
+} ;
 
 /**
  * @brief
  */
 typedef struct vm_operand_stack {
-    uint32_t operand;
-    vm_operand_stack *next;
+    uint16_t frames_count;
+    vm_operand_stack_frame_t *top_frame;
 } vm_operand_stack_t;
 
 /**
  * @brief
  */
-typedef struct vm_frame {
-	vm_class_file_t *class_file;
-	vm_cp_info_t *constant_pool;
-	uint32_t operand_stack_count;
-	vm_operand_stack_t *operand_stack;
-	uint32_t local_variables_count;
-	uint32_t *local_variables;
-} vm_frame_t;
+struct vm_stack_frame {
+    vm_local_variables_list_t *local_variables_list;
+    vm_operand_stack_t *operand_stack;
+    vm_cp_info_t *constant_pool;
+    vm_stack_frame_t *next_frame;
+} ;
 
 /**
  * @brief
  */
-typedef struct vm_frame_stack {
-    vm_frame_t *frame;
-    vm_frame_stack *next;
-} vm_frame_stack_t;
+typedef struct vm_stack {
+    uint16_t frames_count;
+    vm_stack_frame_t *top_frame;
+} vm_stack_t;
 
+void push_into_stack(vm_stack_t *stack, vm_stack_frame_t *new_frame);
+vm_stack_frame_t * pop_from_stack(vm_stack_t *stack);
+
+void push_into_ostack(vm_operand_stack_t *stack, vm_operand_stack_frame_t *new_frame);
+vm_operand_stack_frame_t * pop_from_ostack(vm_operand_stack_t *stack);
 
 #endif /* VM_FRAME_H */
