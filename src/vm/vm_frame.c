fe5b2f8d13b3066e18afc153525c86c674e0400b
DiogoSampaio <di.ltms@gmail.com>
2020-11-08 14:12:32 -0300

    Add implementation of frame stack

---
+++ src/vm/vm_frame.c
@@ -0,0 +1,11 @@
+#include vm_frame.h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+
+#include "vm_file.h"
+#include "utils/vm_target.h"
+#include "lib/vm_string.h"
