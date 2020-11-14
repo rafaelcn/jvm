2e34cecd7e4b28cc412f0e1042b29ea1b796d5e9
Wander-lan <westynger98@gmail.com>
2020-11-14 19:19:33 -0300

    some declarations in vm_opcodes.h

--- src/vm/vm_opcodes.h
+++ src/vm/vm_opcodes.h
@@ -6,8 +6,12 @@
 #include "vm_frame.h"
 
 /**
- * @brief 
+ * @brief The JVM instructions operations.
+ * @param code the code of the instruction.
+ * @param pc The previous PC position.
+ * @param STACK An operands stack.
+ * @returns The actual PC position.
  */
-uint32_t vm_opcodes(uint8_t *bytes, uint32_t pc, vm_stack_t *STACK);
+uint32_t vm_opcodes(uint8_t *code, uint32_t pc, vm_stack_t *STACK);
 
 #endif /* VM_OPCODES_H */
