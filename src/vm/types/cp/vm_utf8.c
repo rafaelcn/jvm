386aad6bb369ad4162645e9c9cec4243d13bbb62
Vinícius Gomes de Souza <vgdsouza@live.com>
2020-11-14 19:53:20 -0300

    Fixing a minor bug in utf8_to_uint16_string
    Signed-off-by: Vinícius Gomes de Souza <vgdsouza@live.com>

--- src/vm/types/cp/vm_utf8.c
+++ src/vm/types/cp/vm_utf8.c
@@ -5,25 +5,33 @@
 uint16_t * vm_utf8_to_uint16_t(uint16_t length, uint8_t *bytes)
 {
     uint16_t *heap = NULL;
+    uint16_t j = 0;
 
-    for (uint16_t i = 0; i < length; i++) {
-        heap = realloc(heap, (sizeof(uint16_t) * (i+1)));
+    for (uint16_t i = 0; i < length;) {
+        heap = realloc(heap, (sizeof(uint16_t) * (j+1)));
 
         if (bytes[i] >> 7) {
             if ((bytes[i] >> 5) & (0x01)) {
                 if (bytes[i] == 0xED) { // Characters with code points above U+FFFF
-                    heap[i] = 0x0000;
+                    i += 6;
                 } else { // '\u0800' to '\uFFFF'
-                    heap[i] = (((bytes[i] & 0x0F) << 12) |
-                              ((bytes[i+1] & 0x3F) << 6) |
-                              (bytes[i+2] & 0x3F));
+                    heap[j] = (
+                        ((bytes[i] & 0x0F) << 12) |
+                        ((bytes[i+1] & 0x3F) << 6) |
+                        (bytes[i+2] & 0x3F)
+                    );
+                    i += 3;
                 }
             } else { // '\u0080' up to '\u07FF' and '\u0000'
-                heap[i] = (((bytes[i] & 0x1F) << 6) | (bytes[i+1] & 0x3F));
+                heap[j] = (((bytes[i] & 0x1F) << 6) | (bytes[i+1] & 0x3F));
+                i += 2;
             }
         } else { // '\u0001' up to '\u007F'
-            heap[i] = bytes[i];
+            heap[j] = bytes[i];
+            i += 1;
         }
+
+        j += 1;
     }
 
     return heap;
