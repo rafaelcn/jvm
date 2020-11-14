6e2cb0983594099a7977a5d8f9c5573effa6109a
Wander-lan <westynger98@gmail.com>
2020-11-14 18:37:14 -0300

    some declarations in vm_frame.h

--- src/vm/vm_frame.c
+++ src/vm/vm_frame.c
@@ -1,11 +1,6 @@
 #include "vm_frame.h"
 
-/**
- * @brief Pushes a stack_frame into the JVM Stack.
- * @param stack A pointer to the JVM Stack.
- * @param new_frame A pointer to the a new frame.
- * @returns Nothing.
- */
+
 void push_into_stack(vm_stack_t *stack, vm_stack_frame_t *new_frame) {
     // the new_frame points towards the former top_frame
     new_frame->next_frame = stack->top_frame;
@@ -17,12 +12,7 @@ void push_into_stack(vm_stack_t *stack, vm_stack_frame_t *new_frame) {
     return;
 }
 
-/**
- * @brief Pushes a operand_stack_frame into the Operands Stack.
- * @param stack A pointer to the Operands Stack.
- * @param new_frame A pointer to the a new frame.
- * @returns Nothing.
- */
+
 void push_into_ostack(vm_operand_stack_t *stack, vm_operand_stack_frame_t *new_frame) {
     // the new_frame points towards the former top_frame
     new_frame->next_frame = stack->top_frame;
@@ -34,11 +24,7 @@ void push_into_ostack(vm_operand_stack_t *stack, vm_operand_stack_frame_t *new_f
     return;
 }
 
-/**
- * @brief Pops the frame at the top of the JVM Stack.
- * @param stack A pointer to the JVM Stack.
- * @returns A pointer to the popped frame.
- */
+
 vm_stack_frame_t * pop_from_stack(vm_stack_t *stack) {
     // temporarily holds the former top_frame
     vm_stack_frame_t *popped_frame = stack->top_frame;
@@ -52,11 +38,7 @@ vm_stack_frame_t * pop_from_stack(vm_stack_t *stack) {
     return popped_frame;
 }
 
-/**
- * @brief Pops the frame at the top of the Operands Stack.
- * @param stack A pointer to the Operands Stack.
- * @returns A pointer to the popped frame.
- */
+
 vm_operand_stack_frame_t * pop_from_ostack(vm_operand_stack_t *stack) {
     // temporarily holds the former top_frame
     vm_operand_stack_frame_t *popped_frame = stack->top_frame;
--- src/vm/vm_frame.h
+++ src/vm/vm_frame.h
@@ -10,7 +10,7 @@ typedef struct vm_operand_stack_frame vm_operand_stack_frame_t;
 typedef struct vm_local_variable_item vm_local_variable_item_t;
 
 /**
- * @brief
+ * @brief A union with the types that will be used for local variables and operands stack.
  */
 typedef union _values {
     int _int;
@@ -20,7 +20,8 @@ typedef union _values {
 } _values_t;
 
 /**
- * @brief
+ * @brief  A single local variable.
+ * used to pass parameters on method invocation.
  */
 struct vm_local_variable_item {
     _values_t value;
@@ -28,15 +29,16 @@ struct vm_local_variable_item {
 };
 
 /**
- * @brief
+ * @brief A list with the number of local variables and a pointer to the first one.
  */
 typedef struct vm_local_variables_list {
     uint16_t local_variables_count;
     vm_local_variable_item_t *first_item;
 } vm_local_variables_list_t;
 
+
 /**
- * @brief
+ * @brief A single frame of operands stack.
  */
 struct vm_operand_stack_frame {
     _values_t value;
@@ -44,7 +46,7 @@ struct vm_operand_stack_frame {
 } ;
 
 /**
- * @brief
+ * @brief A struct to count the number of frames and points to the top_frame.
  */
 typedef struct vm_operand_stack {
     uint16_t frames_count;
@@ -52,7 +54,7 @@ typedef struct vm_operand_stack {
 } vm_operand_stack_t;
 
 /**
- * @brief
+ * @brief The JVM supplies instructions to load constants or values from local variables or fields into the operand stack.
  */
 struct vm_stack_frame {
     vm_local_variables_list_t *local_variables_list;
@@ -62,17 +64,42 @@ struct vm_stack_frame {
 } ;
 
 /**
- * @brief
+ * @brief 
  */
 typedef struct vm_stack {
     uint16_t frames_count;
     vm_stack_frame_t *top_frame;
 } vm_stack_t;
 
+/**
+ * @brief Pushes a stack_frame into the JVM Stack.
+ * @param stack A pointer to the JVM Stack.
+ * @param new_frame A pointer to the a new frame.
+ */
 void push_into_stack(vm_stack_t *stack, vm_stack_frame_t *new_frame);
+
+
+/**
+ * @brief Pops the frame at the top of the JVM Stack.
+ * @param stack A pointer to the JVM Stack.
+ * @returns A pointer to the popped frame.
+ */
 vm_stack_frame_t * pop_from_stack(vm_stack_t *stack);
 
+
+/**
+ * @brief Pushes a operand_stack_frame into the Operands Stack.
+ * @param stack A pointer to the Operands Stack.
+ * @param new_frame A pointer to the a new frame.
+ */
 void push_into_ostack(vm_operand_stack_t *stack, vm_operand_stack_frame_t *new_frame);
+
+
+/**
+ * @brief Pops the frame at the top of the Operands Stack.
+ * @param stack A pointer to the Operands Stack.
+ * @returns A pointer to the popped frame.
+ */
 vm_operand_stack_frame_t * pop_from_ostack(vm_operand_stack_t *stack);
 
 #endif /* VM_FRAME_H */
