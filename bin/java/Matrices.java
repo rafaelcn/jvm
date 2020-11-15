aab1c1bf329b17d6aaf48e3877c841bfd05b6c32
Rafael Campos Nunes <rafaelnunes@engineer.com>
2020-11-15 16:14:42 -0300

    Add test file for matrices
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ bin/java/Matrices.java
@@ -0,0 +1,12 @@
+public class Matrices {
+    public static void main(String args[]) {
+        int a[][] = new int[5][3];
+
+        a[0][0] = 3;
+        a[1][0] = 7;
+        a[1][2] = 2;
+
+        System.out.print("A[0][0]: " + a[0][0] + ", A[1][0]: " + a[1][0]);
+        System.out.print("A[1][2]: " + a[1][2]);
+    }
+}
