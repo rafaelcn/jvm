37f58678b806c8e95fd16740ce62c3302d6926a2
Vinícius Gomes de Souza <vgdsouza@live.com>
2020-11-15 13:35:04 -0300

    Add documentation to constant_pool, print_opcodes
    Signed-off-by: Vinícius Gomes de Souza <vgdsouza@live.com>
    Co-authored-by: Thiago Luis <thiagoluisrpinho@gmail.com>

--- src/vm/vm_print_opcodes.h
+++ src/vm/vm_print_opcodes.h
@@ -5,7 +5,19 @@
 
 #include "vm_constant_pool.h"
 
+/**
+ * @brief A function that converts a OpCode into a JVM Mnemonic.
+ * @param code The code field of a Code Attribute of Method.
+ * @param pc The PC of the current instruction of the method.
+ * @returns Returns the PC of the next instruction.
+ */
 uint32_t vm_print_opcodes_helper(uint8_t *code, uint32_t pc);
+
+/**
+ * @brief A function that prints the mnemonics associated with the OpCodes
+ * of a the main method.
+ * @param class_file A vm_class_file_t structure with a main method.
+ */
 void vm_print_opcodes(vm_class_file_t class_file);
 
 #endif /* VM_PRINT_OPCODES_H */
